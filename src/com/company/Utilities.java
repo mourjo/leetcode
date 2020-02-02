0ecf7eab006443be2e80ebd5d02661ede099411f
Mourjo Sen <sen.mourjo@gmail.com>
2020-02-02 19:28:57 +0530

    Longest palindrome substring

---
+++ src/com/company/Utilities.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Utilities {
+    public static final String RED = "\033[31m";
+    public static final String BLACK = "\033[0m";
+    public static void check (Object actual, Object... expected) {
+        for (Object e : expected)
+            if (actual.equals(e)) {
+                System.out.println(BLACK + "Passed: " + actual);
+                return;
+            }
+
+        System.out.print(RED + "*** Actual: " + actual +" Expected: ");
+        for (Object e : expected) {
+            System.out.print(e + ", ");
+        }
+        System.out.println(BLACK);
+        System.out.println();
+    }
+}
