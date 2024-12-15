d524713c0e838bd42fa74097da560e463e803864
Mourjo Sen <hello@mourjo.me>
2024-12-15 06:38:26 +0100

    Valid Sudoku

---
+++ src/main/java/me/mourjo/y24/arrays/ValidSudoku.java
@@ -0,0 +1,126 @@
+package me.mourjo.y24.arrays;
+
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+
+/* Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+*/
+public class ValidSudoku {
+
+    public static void main(String[] args) {
+        var app = new ValidSudoku();
+        Assertions.assertTrue(
+            app.isValidSudoku(
+                new char[][]{
+                    {'5','3','.','.','7','.','.','.','.'},
+                    {'6','.','.','1','9','5','.','.','.'},
+                    {'.','9','8','.','.','.','.','6','.'},
+                    {'8','.','.','.','6','.','.','.','3'},
+                    {'4','.','.','8','.','3','.','.','1'},
+                    {'7','.','.','.','2','.','.','.','6'},
+                    {'.','6','.','.','.','.','2','8','.'},
+                    {'.','.','.','4','1','9','.','.','5'},
+                    {'.','.','.','.','8','.','.','7','9'}
+                }
+
+            )
+        );
+
+        Assertions.assertFalse(
+            app.isValidSudoku(
+                new char[][]{
+                    {'8','3','.','.','7','.','.','.','.'},
+                    {'6','.','.','1','9','5','.','.','.'},
+                    {'.','9','8','.','.','.','.','6','.'},
+                    {'8','.','.','.','6','.','.','.','3'},
+                    {'4','.','.','8','.','3','.','.','1'},
+                    {'7','.','.','.','2','.','.','.','6'},
+                    {'.','6','.','.','.','.','2','8','.'},
+                    {'.','.','.','4','1','9','.','.','5'},
+                    {'.','.','.','.','8','.','.','7','9'}
+                }
+
+            )
+        );
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        boolean rowLocs[][] = new boolean[9][10];
+        boolean colLocs[][] = new boolean[9][10];
+        boolean miniSqLocs[][][] = new boolean[3][3][10];
+
+        int rowNum = -1;
+        for (char[] rowContents : board){
+            rowNum++;
+            int colNum = -1;
+            for (char element : rowContents) {
+                colNum++;
+                if (element == '.') {
+                    continue;
+                }
+
+                int elem = element - '0';
+                if (rowLocs[rowNum][elem]) {
+                    return false;
+                }
+                rowLocs[rowNum][elem] = true;
+
+                if(colLocs[colNum][elem]) {
+                    return false;
+                }
+                colLocs[colNum][elem] = true;
+
+                int miniSqRow = rowNum/3;
+                int miniSqCol = colNum/3;
+                if(miniSqLocs[miniSqRow][miniSqCol][elem]) {
+                    return false;
+                }
+                miniSqLocs[miniSqRow][miniSqCol][elem] = true;
+            }
+        }
+        return true;
+    }
+
+}
